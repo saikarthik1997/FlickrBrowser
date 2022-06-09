cdd46c11bb7636f187799ca79059fac0fb759e42
Sri Sistla <sri.sistla@ril.com>
2022-06-09 07:50:46 +0530

    download and parsing data

--- app/src/main/java/com/sri/flickrbrowser/MainActivity.kt
+++ app/src/main/java/com/sri/flickrbrowser/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sri.flickrbrowser
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -12,13 +13,14 @@ import android.view.Menu
 import android.view.MenuItem
 import com.sri.flickrbrowser.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
+    GetFlickrJsonData.OnDataAvailable{
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG,"OnCreate called")
+        Log.d(TAG, "OnCreate called")
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -29,6 +31,11 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        val url =
+            createUri("https://api.flickr.com/services/feeds/photos_public.gne", "", "en-us", true)
+
+        val getRawData = GetRawData(this)
+        getRawData.execute(url)
 
 //        binding.fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -37,14 +44,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        Log.d(TAG,"onCreateOptionsMenu called")
+        Log.d(TAG, "onCreateOptionsMenu called")
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Log.d(TAG,"onOptionsItemSelected called")
+        Log.d(TAG, "onOptionsItemSelected called")
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -59,7 +66,44 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
-    companion object{
-        private const val TAG="MainActivity";
+
+    companion object {
+        private const val TAG = "MainActivity";
+    }
+
+    override fun onDownloadComplete(data: String, status: DownloadStatus) {
+        if (status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete called")
+
+            val getFlickrJsonData = GetFlickrJsonData(this)
+            getFlickrJsonData.execute(data)
+        } else {
+            // download failed
+            Log.d(TAG, "onDownloadComplete failed with status $status. Error message is: $data")
+        }
+    }
+
+    private fun createUri(
+        baseURL: String,
+        searchCriteria: String,
+        lang: String,
+        matchAll: Boolean
+    ): String {
+        Log.d(TAG, ".createUri starts")
+
+        return Uri.parse(baseURL).buildUpon().appendQueryParameter("tags", searchCriteria)
+            .appendQueryParameter("tagmode", if (matchAll) "ALL" else "ANY")
+            .appendQueryParameter("lang", lang).appendQueryParameter("format", "json")
+            .appendQueryParameter("nojsoncallback", "1").build().toString()
+    }
+
+    override fun onDataAvailable(data: List<Photo>) {
+        Log.d(TAG, ".onDataAvailable called")
+        //  flickrRecyclerViewAdapter.loadNewData(data)
+        Log.d(TAG, ".onDataAvailable ends")
+    }
+
+    override fun onError(exception: Exception) {
+        Log.e(TAG, "onError called with ${exception.message}")
     }
 }
